08dc005f59d332dc8e836d2606264d46ad6ab88b
Jeremiah Bagula <jbagula@awninc.com>
2016-02-24 11:36:04 -0700

    Added in the old code currently running on the goggles

--- NeoPixel_Ring_Goggles.ino
+++ NeoPixel_Ring_Goggles.ino
@@ -14,6 +14,12 @@
 // Start the libraries
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIX, NEO_PIN, NEO_GRB + NEO_KHZ800);
 
+// Part of the old code
+uint8_t  mode   = 0; // Current animation effect
+uint8_t  offset = 0; // Position of spinny eyes
+uint32_t color  = 0xFF0000; // Start red
+uint32_t prevTime;
+
 // Setup for the bootstrap
 void setup() 
 {
@@ -24,10 +30,61 @@ void setup()
 
   // Start the NeoPixels
   pixels.begin();
+
+  //  Set the brightness to 1/3
+  pixels.setBrightness(85);
 }
 
 // MCU loop
 void loop() 
 {
-  // TODO: Fill the body with combination of old a new code  
+  // TODO: Fill the body with combination of old a new code
+
+//  uint8_t  i;
+//  uint32_t currTime;
+
+//  switch(mode) 
+//  {
+//
+//   case 0: // Random sparks - just one LED on at a time!
+//    i = random(32);
+//    pixels.setPixelColor(i, color);
+//    pixels.show();
+//    delay(10);
+//    pixels.setPixelColor(i, 0);
+//    break;
+// 
+//   case 1: // Spinny wheels (8 LEDs on at a time)
+//    for(i=0; i<16; i++) 
+//    {
+//      uint32_t c = 0;
+//      if(((offset + i) & 7) < 2) c = color; // 4 pixels on...
+//      pixels.setPixelColor(   i, c); // First eye
+//      pixels.setPixelColor(31-i, c); // Second eye (flipped)
+//    }
+//    pixels.show();
+//    offset++;
+//    delay(50);
+//    break;
+//  }
+
+//  currTime = millis();
+//  
+//  if((currTime - prevTime) > 8000) 
+//  {                                // Every 8 seconds...
+//    mode++;                        // Next mode
+//    if(mode > 1) 
+//    {                              // End of modes?
+//      mode = 0;                    // Start modes over
+//      color >>= 8;                 // Next color R->G->B
+//      
+//      if(!color) 
+//      {
+//        color = 0xFF0000;          // Reset to red
+//      }
+//    }
+//    for(i=0; i<32; i++) pixels.setPixelColor(i, 0);
+//    prevTime = currTime;
+//  }
+
 }
